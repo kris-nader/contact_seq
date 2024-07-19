5d18ee3ead15fe7d12da16ffc68a5b6bda4740b0
Kristen Michelle Nader <61286548+kris-nader@users.noreply.github.com>
2024-07-19 11:09:47 +0300

    Update create_ref.sh

--- create_ref.sh
+++ create_ref.sh
@@ -11,8 +11,8 @@ cp Homo_sapiens.GRCh38.dna.primary_assembly.fa hg38_fasta.fa
 cp Homo_sapiens.GRCh38.112.gtf hg38_gtf.gtf
 
 ## append the fasta for the 2 genes
-cat ./kristen_contact/GFP.txt >> hg38_fasta.fa
-cat ./kristen_contact/mScarlet.txt  >> hg38_fasta.fa
+cat ./GFP.txt >> hg38_fasta.fa
+cat ./mScarlet.txt  >> hg38_fasta.fa
 
 ## check if they have been appended to the fasta file-- there should be 2 new genes so 194+2
 grep ">" hg38_fasta.fa
@@ -20,19 +20,19 @@ grep -c "^>" hg38_fasta.fa ## 196
 grep -c "^>" Homo_sapiens.GRCh38.dna.primary_assembly.fa ## 194
 
 ## check length of the fasta genes
-tail -n +2 ./kristen_contact/GFP.txt | wc | awk '{print $3-$1}' ## 717
-tail -n +2 ./kristen_contact/mScarlet.txt | wc | awk '{print $3-$1}' ## 690
+tail -n +2 ./GFP.txt | wc | awk '{print $3-$1}' ## 717
+tail -n +2 ./mScarlet.txt | wc | awk '{print $3-$1}' ## 687 remember the stop codon!!
 
 ## check gtf format
 echo -e "GFP\tcustom\tgene\t1\t717\t.\t+\t.\tgene_id \"id_GFP\"; gene_name \"GFP\"; gene_biotype \"protein_coding\";"
 echo -e "GFP\tcustom\texon\t1\t717\t.\t+\t.\tgene_id \"id_GFP\"; gene_name \"GFP\"; gene_biotype \"protein_coding\";"
-echo -e "mscarlet\tcustom\tgene\t1\t690\t.\t+\t.\tgene_id \"id_mscarlet\"; gene_name \"mscarlet\"; gene_biotype \"protein_coding\";"
-echo -e "mscarlet\tcustom\texon\t1\t690\t.\t+\t.\tgene_id \"id_mscarlet\"; gene_name \"mscarlet\"; gene_biotype \"protein_coding\";"
+echo -e "mscarlet\tcustom\tgene\t1\t687\t.\t+\t.\tgene_id \"id_mscarlet\"; gene_name \"mscarlet\"; gene_biotype \"protein_coding\";"
+echo -e "mscarlet\tcustom\texon\t1\t687\t.\t+\t.\tgene_id \"id_mscarlet\"; gene_name \"mscarlet\"; gene_biotype \"protein_coding\";"
 ## add in gtf format exon and gene
 echo -e "GFP\tcustom\tgene\t1\t717\t.\t+\t.\tgene_id \"id_GFP\"; gene_name \"GFP\"; gene_biotype \"protein_coding\";"  >> hg38_gtf.gtf
 echo -e "GFP\tcustom\texon\t1\t717\t.\t+\t.\tgene_id \"id_GFP\"; gene_name \"GFP\"; gene_biotype \"protein_coding\";"  >> hg38_gtf.gtf 
-echo -e "mscarlet\tcustom\tgene\t1\t690\t.\t+\t.\tgene_id \"id_mscarlet\"; gene_name \"mscarlet\"; gene_biotype \"protein_coding\";" >> hg38_gtf.gtf
-echo -e "mscarlet\tcustom\texon\t1\t690\t.\t+\t.\tgene_id \"id_mscarlet\"; gene_name \"mscarlet\"; gene_biotype \"protein_coding\";"  >> hg38_gtf.gtf 
+echo -e "mscarlet\tcustom\tgene\t1\t687\t.\t+\t.\tgene_id \"id_mscarlet\"; gene_name \"mscarlet\"; gene_biotype \"protein_coding\";" >> hg38_gtf.gtf
+echo -e "mscarlet\tcustom\texon\t1\t687\t.\t+\t.\tgene_id \"id_mscarlet\"; gene_name \"mscarlet\"; gene_biotype \"protein_coding\";"  >> hg38_gtf.gtf 
 
 ## .fa and .gtf to .fa.gz and .gtf.gz
 gzip -k hg38_gtf.gtf
@@ -41,15 +41,15 @@ gzip -k hg38_fasta.fa
 ## run mkref
 split-pipe \
 --mode mkref \
---genome_name hg38 \
+--genome_name  \
 --fasta hg38_fasta.fa.gz \
 --genes hg38_gtf.gtf.gz \
---output_dir ./newgenome_no_genome_name/
+--output_dir ./new_genome_scarlet_fix_empty/
 
-## unzup and check if the 2 genes are there
-gzip -dk ./newgenome_no_genome_name/process/genome.fas.gz
-grep "GFP" ./newgenome_no_genome_name/process/genome.fas
-grep "mScarlet" ./newgenome_no_genome_name/process/genome.fas
+## unzip and check if the 2 genes are there
+gzip -dk ./new_genome_scarlet_fix/process/genome.fas.gz
+grep "GFP" ./new_genome_scarlet_fix/process/genome.fas
+grep "mScarlet" ./new_genome_scarlet_fix/process/genome.fas
 
 
 
